05ba7a801a2f4ec034a134a0dc6bb63fe79a4079
Valérie Gauvin <valerie.gauvin@polymtl.ca>
2023-02-04 20:44:02 -0500

    first commit

---
+++ journal/app/src/main/assets/script.js
@@ -0,0 +1,4 @@
+function changer_page(){
+    window.location = "/Users/valeriegauvin/Documents/École/PolyHacks2023/Polyhacks/journal/app/src/main/assets/Symptômes.html"
+    window.location.href= "/Users/valeriegauvin/Documents/École/PolyHacks2023/Polyhacks/journal/app/src/main/assets/Symptômes.html"
+}
