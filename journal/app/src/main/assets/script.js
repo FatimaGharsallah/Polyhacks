eed073eabd9a39302ba448a2f3616c6e521f6303
Valérie Gauvin <valerie.gauvin@polymtl.ca>
2023-02-04 22:02:19 -0500

    deuxieme commit

--- journal/app/src/main/assets/script.js
+++ journal/app/src/main/assets/script.js
@@ -1,4 +1,47 @@
-function changer_page(){
-    window.location = "/Users/valeriegauvin/Documents/École/PolyHacks2023/Polyhacks/journal/app/src/main/assets/Symptômes.html"
-    window.location.href= "/Users/valeriegauvin/Documents/École/PolyHacks2023/Polyhacks/journal/app/src/main/assets/Symptômes.html"
+
+function save() {	
+	var checkbox = document.getElementById("checkbox");
+    localStorage.setItem("checkbox", checkbox.checked);	
 }
+
+//for loading
+var checked = JSON.parse(localStorage.getItem("checkbox"));
+    document.getElementById("checkbox").checked = checked;
+
+
+// Fonction qui permet d'enregistrer les informations entrées à l'étape 2.
+function validationEtape2() {    
+    toggleText('etape3') 
+    coursEte=false;
+    stageFait=false;
+    nbreCreditParSession=0;
+    nbreSession=0;
+    nbreSessionsPlanifiees = 0;
+    document.getElementById("erreurEtape2").innerHTML = "";
+    // Validation de la réponse à la question du stage obligatoire   
+    if(document.getElementById('ouiStage').checked) {
+      stageFait = true;   
+    }   
+    else if(document.getElementById('nonStage').checked) {   
+      stageFait = false;
+    }   
+    else {   
+      document.getElementById("erreurEtape2").innerHTML = "Il manque au moins une réponse";   
+    }
+    // Validation de la réponse à la question pour les cours d'été
+    if(document.getElementById('ouiEte').checked) {
+      coursEte = true;
+    }
+    else if(document.getElementById('nonEte').checked) {  
+      coursEte=false;
+    }   
+    else {   
+      document.getElementById("erreurEtape2").innerHTML = "Il manque au moins une réponse";  
+      toggleText('etape3')  
+    } 
+    nbreCreditParSession = parseInt(document.getElementById("nbreCours").value);
+    nbreSession = parseInt(document.getElementById("nbreSession").value);
+    nbreSessionsPlanifiees = Math.ceil((120-nbreCreditsReussis)/nbreCreditParSession);   
+  }
+
+  
